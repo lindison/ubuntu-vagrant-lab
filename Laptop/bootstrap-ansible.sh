865bb6ddb5164cdeedfb6d9d5c2ee223359c41bd
Lindis <me@lindison.com>
2017-05-12 08:04:59 -0700

    updates

--- Laptop/bootstrap-ansible.sh
+++ Laptop/bootstrap-ansible.sh
@@ -10,9 +10,6 @@ apt-get install -y tree ansible sshpass
 touch /etc/ansible/hosts
 cat >> /etc/ansible/hosts <<EOL
 
-[leader]
-node-0
-
 [workers]
 node-1
 node-2
@@ -23,8 +20,17 @@ ansible_ssh_pass=vagrant
 
 EOL
 
-# 
-copy /vagrant/ansible.sh /root/ansible.sh
+# Define .ssh/known_hosts
+cp /vagrant/ansible.sh /root/
+ssh-keyscan node-0 >> /root/.ssh/known_hosts
+ssh-keyscan node-1 >> /root/.ssh/known_hosts
+ssh-keyscan node-2 >> /root/.ssh/known_hosts
+
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+ansible node-1 -s -m file -a "path=/root/.ssh/ state=directory"
+ansible node-1 -s -m copy -a "src=/root/.ssh/authorized_keys dest=/root/.ssh/authorized_keys owner=root group=root mode=644"
+ansible node-2 -s -m file -a "path=/root/.ssh/ state=directory"
+ansible node-2 -s -m copy -a "src=/root/.ssh/authorized_keys dest=/root/.ssh/authorized_keys owner=root group=root mode=644"
 
 # Install HTOP
 apt-get install htop -y
